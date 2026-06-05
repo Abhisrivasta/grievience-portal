5f1169d7c7001d904e453b5941f81553315ce767
Abhishek Srivastava <163029373+Abhisrivasta@users.noreply.github.com>
2026-06-05 22:09:16 +0530

    fix api issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "https://grievience-portal-backend-production.up.railway.app/api";
+    : "https://grievience-portal-backend.onrender.com/api";
 
 
 const api = axios.create({
