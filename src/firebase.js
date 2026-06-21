08c8ce78eb4aee2639e9e63929973b119fcedfdc
Abhishek Srivastava <as6338830@gmail.com>
2026-06-21 14:09:58 +0530

    added firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY  || "AIzaSyC8gJPhkiDz5LvEeFV_ZMOytb3t4KlClI8",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "grivienceportal.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "grivienceportal",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
